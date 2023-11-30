5f313720d0ad3a9e9044243b216446a35fb95231
saideepsamineni <saminenisaideepchowdary@gmail.com>
2023-11-30 00:11:31 -0500

    modified mongo db schema

--- pictures/schema.js
+++ pictures/schema.js
@@ -3,6 +3,9 @@ const pictureSchema = new mongoose.Schema({
     _id : { type: String, required: true, unique: true },
     postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     description: { type: String },
+    public: { type: Boolean, default:false},
+    likes : {type : Number, default: 0},
+    comments : [{ type: String}],
     timestamp: { type: Date, default: Date.now }
   },
   { collection: "pictures" });
